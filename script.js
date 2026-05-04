16aaf197140076e23abb5436afe6049ae593d3d7
marthasai <158295414+marthasai@users.noreply.github.com>
2026-05-04 20:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTimer() {
+  const now = new Date();
+  document.getElementById("timer").innerText = now.toString();
+}
+
+setInterval(updateTimer, 1000);
+
+updateTimer();
